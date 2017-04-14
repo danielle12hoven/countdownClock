6271fae6ec07cd8c6323ccfd5967731586bbf3cb
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-04-14 08:42:33 -0400

    added state

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline})
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div>Countdown to December 25, 2017</div>
+        <div className="App-title">Countdown to {this.state.deadline}</div>
         <div>
-          <div>14 Days</div>
-          <div>30 Hours</div>
-          <div>15 Minutes</div>
-          <div>20 Seconds</div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
-        <input placeholder="new date" />
-        <button>Submit</button>
+        <input
+          onChange={event => this.setState({newDeadline: event.target.value})}
+          placeholder='new date'
+        />
+        <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
 
