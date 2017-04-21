6d11764cf43cf707befac6a4eacd0ec63091292f
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-04-21 07:26:23 -0400

    music

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './Profile.jsx';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query:''
+      query:'',
+      artist: null
     }
   }
-
-
 
   search() {
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?'
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}
-                      &type=artist&limit=1`;
-    console.log('FETCH_URL', FETCH_URL)
-
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log('FETCH_URL', FETCH_URL);
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      console.log('artist', artist)
+      this.setState({artist});
+    });
   }
 
 
@@ -49,10 +57,9 @@ class App extends Component {
           </InputGroup>
         </FormGroup>
 
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
+          <Profile
+            artist={this.state.artist}
+          />
 
         <div className="Gallery">Gallery</div>
       </div>
