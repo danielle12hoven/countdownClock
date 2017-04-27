5368069269aa1d09cc4c1098ebfe68ab30e0744e
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-04-27 14:58:00 -0400

    previews working

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import Profile from './Profile.jsx';
+import Gallery from './Gallery.jsx';
 
 
 class App extends Component {
@@ -9,15 +10,17 @@ class App extends Component {
     super(props);
     this.state = {
       query:'',
-      artist: null
+      artist: null,
+      tracks:[]
     }
   }
 
   search() {
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?'
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    console.log('FETCH_URL', FETCH_URL);
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+
     fetch(FETCH_URL, {
       method: 'GET'
     })
@@ -26,6 +29,17 @@ class App extends Component {
       const artist = json.artists.items[0];
       console.log('artist', artist)
       this.setState({artist});
+
+      FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+      fetch(FETCH_URL, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log('artist\'s top tracks:', json)
+        const tracks = json.tracks;
+        this.setState({tracks});
+      })
     });
   }
 
@@ -65,9 +79,9 @@ class App extends Component {
                 artist={this.state.artist}
               />
 
-              <div className="Gallery">
-                Gallery
-              </div>
+              <Gallery
+                tracks={this.state.tracks}
+                />
             </div>
           : <div></div>
         }
---
+++ musicmaster/src/Gallery.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Gallery extends Component {
+  playAudio(previewURL) {
+    let audio = new Audio(previewURL)
+    audio.play()
+  }
+
+  render() {
+    const tracks = this.props.tracks;
+    return (
+      <div>
+        {tracks.map((track, k) => {
+          console.log('track', track);
+          const trackImg = track.album.images[0].url;
+          return (
+            <div
+              key={k}
+              className="track"
+              onClick={() => this.playAudio(track.preview_url)}
+            >
+              <img
+                src={trackImg}
+                className="track-img"
+                alt="track"
+              />
+              <p className="track-text">
+                {track.name}
+              </p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Gallery;
